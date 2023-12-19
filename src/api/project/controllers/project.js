b793c6b02ab63613946c87a8ea28e8ba7161e2cb
Stephen Leachman <globalumbrella16@gmail.com>
2023-12-19 16:37:08 -0500

    update

--- src/api/project/controllers/project.js
+++ src/api/project/controllers/project.js
@@ -12,13 +12,24 @@ module.exports = createCoreController('api::project.project', ({strapi}) => ({
     async findOne(ctx) {
         const { slug } = ctx.params;
 
-        const entity = await strapi.db.query('api::project.project').findOne({
-            where: { slug }
-        });
+    //     const entity = await strapi.db.query('api::project.project').findOne({
+    //         where: { slug }
+    //     });
 
-        // @ts-ignore
-        const sanitizedEntity = await this.sanitizeOutput(entity);
+    //     // @ts-ignore
+    //     const sanitizedEntity = await this.sanitizeOutput(entity);
 
-        return this.transformResponse(sanitizedEntity);
-    }
-}));
+    //     return this.transformResponse(sanitizedEntity);
+    // }
+
+    const query = {
+        filters: { slug },
+        ...ctx.query,
+      };
+  
+      const project = await strapi.entityService.findMany("api::project.project", query);
+  
+      return this.transformResponse(project[0]);
+    },
+  }));
+// }));
