412bd2afef807aaa913f899fea867050643b69d6
Stephen Leachman <globalumbrella16@gmail.com>
2024-10-28 17:46:34 -0400

    test

--- src/api/blog/controllers/blog.js
+++ src/api/blog/controllers/blog.js
@@ -10,23 +10,13 @@ module.exports = createCoreController('api::blog.blog', ({strapi}) => ({
     async findOne(ctx) {
         const { slug } = ctx.params;
 
-        // const entity = await strapi.db.query('api::blog.blog').findOne({
-        //     where: { slug }
-        // });
+        const entity = await strapi.db.query('api::blog.blog').findOne({
+            where: { slug }
+        });
 
         // @ts-ignore
-        // const sanitizedEntity = await this.sanitizeOutput(entity);
+        const sanitizedEntity = await this.sanitizeOutput(entity);
 
-        // return this.transformResponse(sanitizedEntity);
-
-        
-        const query = {
-            filters: { slug },
-            ...ctx.query,
-          };
-
-        const blog = await strapi.entityService.findMany("api::blog.blog", query);
-  
-        return this.transformResponse(blog[0]);
+        return this.transformResponse(sanitizedEntity);
     }
 }));
